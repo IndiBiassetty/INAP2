b9a83bddac2c74fbd06f8719660d38484f51dcdf
Indiana <Indiana@INDIANA>
2016-05-21 21:27:11 -0600

    Se agrega logo a la interfaz

--- src/p2/TecMarketInterfaz.java
+++ src/p2/TecMarketInterfaz.java
@@ -4,58 +4,67 @@ import java.awt.Dimension;
 
 public class TecMarketInterfaz extends javax.swing.JFrame {
 
-    
-    
     public TecMarketInterfaz() {
         initComponents();
+        //Esto hace que se le pueda dar tamaño propio a los paneles de la interfaz.
         setLayout(null);
         setResizable(false);
-        setSize(700,500);
-        setLocation(300,100);   
-        panelPrincipal.setBounds(0, 0, 700, 500);
+        //Tamaño del jframe principal
+        setSize(800,600);
+        //Ubicacion en la pantalla jframe
+        setLocation(300,100);
+        //Ubicacion y tamaño del panelPrincipal
+        panelPrincipal.setBounds(0, 0, 800, 600);
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollBar1 = new javax.swing.JScrollBar();
         panelPrincipal = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(51, 255, 51));
         setResizable(false);
         setSize(new java.awt.Dimension(700, 500));
 
-        panelPrincipal.setBackground(new java.awt.Color(255, 102, 204));
+        panelPrincipal.setBackground(new java.awt.Color(255, 255, 255));
         panelPrincipal.setDoubleBuffered(false);
-        panelPrincipal.setMaximumSize(new java.awt.Dimension(700, 500));
-        panelPrincipal.setMinimumSize(new java.awt.Dimension(700, 500));
-        panelPrincipal.setPreferredSize(new java.awt.Dimension(700, 500));
+        panelPrincipal.setMaximumSize(new java.awt.Dimension(800, 600));
+        panelPrincipal.setMinimumSize(new java.awt.Dimension(800, 600));
+        panelPrincipal.setPreferredSize(new java.awt.Dimension(800, 600));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/titulo.jpg"))); // NOI18N
 
         javax.swing.GroupLayout panelPrincipalLayout = new javax.swing.GroupLayout(panelPrincipal);
         panelPrincipal.setLayout(panelPrincipalLayout);
         panelPrincipalLayout.setHorizontalGroup(
             panelPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addGroup(panelPrincipalLayout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 754, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(23, Short.MAX_VALUE))
         );
         panelPrincipalLayout.setVerticalGroup(
             panelPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 500, Short.MAX_VALUE)
+            .addGroup(panelPrincipalLayout.createSequentialGroup()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 519, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(panelPrincipal, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 320, Short.MAX_VALUE))
+            .addComponent(panelPrincipal, javax.swing.GroupLayout.DEFAULT_SIZE, 800, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(panelPrincipal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 22, Short.MAX_VALUE))
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
@@ -70,6 +79,8 @@ public class TecMarketInterfaz extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollBar jScrollBar1;
     private javax.swing.JPanel panelPrincipal;
     // End of variables declaration//GEN-END:variables
 }
