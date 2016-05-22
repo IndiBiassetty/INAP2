3672541fd866249e0cf22db42578831ff324ace4
Indiana <Indiana@INDIANA>
2016-05-21 20:11:04 -0600

    Se logra ponerle tamaño y ubicacion a los panel sin que se desconfiguren

--- src/p2/TecMarketInterfaz.java
+++ src/p2/TecMarketInterfaz.java
@@ -1,27 +1,61 @@
 package p2;
 
+import java.awt.Dimension;
+
 public class TecMarketInterfaz extends javax.swing.JFrame {
 
     
+    
     public TecMarketInterfaz() {
         initComponents();
+        setLayout(null);
+        setResizable(false);
+        setSize(700,500);
+        setLocation(300,100);   
+        panelPrincipal.setBounds(0, 0, 700, 500);
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        panelPrincipal = new javax.swing.JPanel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(51, 255, 51));
+        setResizable(false);
+        setSize(new java.awt.Dimension(700, 500));
+
+        panelPrincipal.setBackground(new java.awt.Color(255, 102, 204));
+        panelPrincipal.setDoubleBuffered(false);
+        panelPrincipal.setMaximumSize(new java.awt.Dimension(700, 500));
+        panelPrincipal.setMinimumSize(new java.awt.Dimension(700, 500));
+        panelPrincipal.setPreferredSize(new java.awt.Dimension(700, 500));
+
+        javax.swing.GroupLayout panelPrincipalLayout = new javax.swing.GroupLayout(panelPrincipal);
+        panelPrincipal.setLayout(panelPrincipalLayout);
+        panelPrincipalLayout.setHorizontalGroup(
+            panelPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        panelPrincipalLayout.setVerticalGroup(
+            panelPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 500, Short.MAX_VALUE)
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(panelPrincipal, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 320, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(panelPrincipal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 22, Short.MAX_VALUE))
         );
 
         pack();
@@ -36,5 +70,6 @@ public class TecMarketInterfaz extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel panelPrincipal;
     // End of variables declaration//GEN-END:variables
 }
