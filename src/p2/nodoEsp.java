3082aac397602597aa2ab11c9a898efa576f0557
Indiana <Indiana@INDIANA>
2016-06-02 19:48:28 -0600

    Se vuelve a subir el grafo

---
+++ src/p2/nodoEsp.java
@@ -0,0 +1,33 @@
+/*
+ * En esta clse se creara la lista de supermercados con los cuales se trabajara
+ */
+package p2;
+
+
+import java.util.*;
+
+public class nodoEsp{
+ 	public String   SuperM;				//Guarda el supermercado
+ 	public boolean  Indicador;			//Booleado encargado de actividad
+ 	public LinkedList <nodoN> Rutas;                //Lista de Rutas
+
+        
+        
+  ///////////////Constructor//////////////
+ 	public nodoEsp(){
+		SuperM     = null;
+		Indicador  = false;
+		Rutas      = new LinkedList<nodoN>();
+	}
+
+	public nodoEsp(String s){
+			this.SuperM      = s;
+			this.Indicador   = false;
+			this.Rutas       = new LinkedList<nodoN>();
+	}
+
+	public String ListaSuper(){
+		return "\n>>> Nombre de Supermercado ="+this.SuperM +" " +"\n>>>";
+	}
+
+}
